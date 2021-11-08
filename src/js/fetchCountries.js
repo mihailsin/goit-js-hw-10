7355f33141738ab6235b30e1f88c0629fb16dd7e
Mihail Sinkov <mihailsin88@gmail.com>
2021-11-08 22:38:11 +0200

    makes fetch function throw error if error during fetch acquired

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,9 @@
 export const fetchCountries = function (name) {
   const filters = 'fields=name,capital,population,flags,languages';
   return fetch(`https://restcountries.com/v3.1/name/${name}?${filters}`).then(response => {
+    if (!response.ok) {
+      throw new Error(onFetchError);
+    }
     return response.json();
   });
 };
